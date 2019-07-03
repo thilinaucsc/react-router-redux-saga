65c479ffee14ad11f6e27e2489cd972f789ae6a3
Thilina Mudalige <thilinamu@mobitel.lk>
2019-07-03 14:43:02 +0530

    redux route and react route added

--- src/index.js
+++ src/index.js
@@ -7,11 +7,28 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStores';
 
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from 'history'
+import indexRoutes from "./routes/index.jsx";
+/*
+import {createBrowserHistory} from "history";
+import { syncHistoryWithStore } from 'react-router-redux';*/
+
+const hist = createBrowserHistory();
 const store = configureStore();
+
+// Create an enhanced history that syncs navigation events with the store
+// const history = syncHistoryWithStore(hist, store);
 
 ReactDOM.render(
     <Provider store={store}>
-    <App />,
+        <Router history={hist}>
+            <Route path="/" component={App}>
+                {indexRoutes.map((prop, key) => {
+                    return <Route path={prop.path} component={prop.component} key={key}/>;
+                })}
+            </Route>
+        </Router>
     </Provider>, document.getElementById('root')
 );
 
